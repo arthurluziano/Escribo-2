cc1bac74d0ebfab83ad040ac34b82678f252f1a3
Arthur Luziano <luzianoarthur@gmail.com>
2023-12-11 17:42:17 -0300

    update: user datas checks

--- app.js
+++ app.js
@@ -7,10 +7,64 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
+// Configurar o Express para leitura de Json.
+app.use(express.json());
+
+// Models
+const User = require("./models/User");
+
+// Rota pública.
 app.get("/", (req, res) => {
     res.status(200).json({ msg: "Bem vindo a nossa API." });
 });
 
+// Registro de Usuários.
+app.post("/auth/register", async (req, res) => {
+
+    const { name, email, password, confirmpassword, telephone } = req.body;
+
+    // Validações.
+    if (!name) {
+        return res.status(422).json({ msg: "O nome é obrigatório!" });
+    }
+
+    if (!email) {
+        return res.status(422).json({ msg: "O email é obrigatório!" });
+    }
+
+    if (!password) {
+        return res.status(422).json({ msg: "A senha é obrigatória!" });
+    }
+
+    if (!telephone) {
+        return res.status(422).json({ msg: "Por favor, insira um telefone!" });
+    }
+
+    if (password !== confirmpassword) {
+        return res.status(422).json({ msg: "As senhas não conferem!" });
+    }
+
+    // Confirmação de existência do Usuário.
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+        return res.status(422).json({ msg: "Por favor, utilize outro email!" });
+    }
+
+    // Criação da senha.
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Criação do usuário.
+    const user = new User({
+        name,
+        email,
+        password,
+        telephone,
+    });
+});
+
+// Credenciais.
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 

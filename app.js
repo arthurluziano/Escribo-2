ce9f06d700b3507e6b6489b6eba2c7d7af7f98f4
Arthur Luziano <luzianoarthur@gmail.com>
2023-12-11 18:34:12 -0300

    add: create validations and token scheme

--- app.js
+++ app.js
@@ -18,6 +18,49 @@ app.get("/", (req, res) => {
     res.status(200).json({ msg: "Bem vindo a nossa API." });
 });
 
+// Rota privada.
+app.get("/user/:id", checkToken, async (req, res) => {
+
+    const id = req.params.id;
+
+    // Confirmação de existência do Usuário.
+    const user = await User.findById(id, "-password");
+
+    // Validações.
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado!" });
+    }
+
+    res.status(200).json({ user });
+
+});
+
+// Checagem de acesso somente se o Token for válido.
+function checkToken(req, res, next) {
+
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ msg: "Acesso negado!" });
+    }
+
+    try {
+
+        const secret = process.env.SECRET;
+
+        jwt.verify(token, secret);
+
+        next();
+
+    } catch (error) {
+
+        res.status(400).json({ msg: "Token invalido!" });
+
+    }
+
+}
+
 // Registro de Usuários.
 app.post("/auth/register", async (req, res) => {
 
@@ -59,9 +102,72 @@ app.post("/auth/register", async (req, res) => {
     const user = new User({
         name,
         email,
-        password,
+        password: passwordHash,
         telephone,
     });
+
+    try {
+
+        await user.save();
+
+        res.status(201).json({ msg: "Usuário criado com sucesso!" });
+
+    } catch (error) {
+        console.log(error);
+
+        res
+            .status(500)
+            .json({ msg: "Tivemos um problema no servidor, tente novamente mais tarde!" });
+    }
+});
+
+// Login de Usuários.
+app.post("/auth/login", async (req, res) => {
+
+    const { email, password } = req.body;
+
+    // Validações.
+    if (!email) {
+        return res.status(422).json({ msg: "O email é obrigatório!" });
+    }
+
+    if (!password) {
+        return res.status(422).json({ msg: "A senha é obrigatória!" });
+    }
+
+    // Confirmação de existência do Usuário.
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado!" });
+    }
+
+    // Confirmação da senha utilizada.
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+        return res.status(422).json({ msg: "Senha inválida!" });
+    }
+
+    try {
+
+        const secret = process.env.SECRET;
+
+        const token = jwt.sign({
+            id: user._id
+        }, secret,
+        );
+
+        res.status(200).json({ msg: "Autenticação realizada com sucesso", token });
+
+    } catch (err) {
+        console.log(err);
+
+        res
+            .status(500)
+            .json({ msg: "Tivemos um problema no servidor, tente novamente mais tarde!" });
+    }
+
 });
 
 // Credenciais.
